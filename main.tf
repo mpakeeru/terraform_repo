a62868a8f23e5bd807acab53ddc3f5ff3f96f66b
mpakeeru <136519474+mpakeeru@users.noreply.github.com>
2024-03-30 11:55:24 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,3 +16,7 @@ resource "aws_instance" "appserver" {
   ami = "ami-019f9b3318b7155c5"
   instance_type = "t2.micro"
 }
+resource "aws_instance" "dbserver" {
+  ami = "ami-019f9b3318b7155c5"
+  instance_type = "t2.micro"
+}
